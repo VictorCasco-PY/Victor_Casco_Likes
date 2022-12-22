1acda80313280e716c52f541320c97fff6dbebcf
VictorCasco-PY <cascoalmada@gmail.com>
2022-12-22 19:23:48 -0300

    funcion like mejorada

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-var like1 = document.querySelector("#like1");
-var like2 = document.querySelector("#like2");
-var like3 = document.querySelector("#like3");
-var likeCounter1 = 0;
-var likeCounter2 = 0;
-var likeCounter3 = 0;
+//index
+var likes = [0, 0, 0];
+var spans = [
+    document.querySelector("#post-1"),
+    document.querySelector("#post-2"),
+    document.querySelector("#post-3")
+];
 
-function addLike1() {
-    likeCounter1++;
-    like1.innerText = likeCounter1 + ' like(s)';
-}
-function addLike2() {
-    likeCounter2++;
-    like2.innerText = likeCounter2 + ' like(s)';
-}
-function addLike3() {
-    likeCounter3++;
-    like3.innerText = likeCounter3 + ' like(s)';
+function like(id){
+    likes[id]++;
+    spans[id].innerHTML = likes[id] + " like(s)";
 }
